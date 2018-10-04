2e0adc4fc89e3ec646dba1ad682f3742122c7b0e
Shhhpoulpe <43034956+Shhhpoulpe@users.noreply.github.com>
2018-10-04 14:09:49 +0200

    Update v_sommaireComptable.php

--- gsbMVC_V0/vues/v_sommaireComptable.php
+++ gsbMVC_V0/vues/v_sommaireComptable.php
@@ -9,7 +9,7 @@
       </div>  
         <ul id="menuList">
 			<li >
-				  Visiteur :<br>
+				  Comptable :<br>
 				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
 			</li>
            <li class="smenu">
