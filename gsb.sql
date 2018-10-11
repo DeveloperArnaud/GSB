5de4de24020519cc2a5eca48dc7502f61bf02f5e
Shhhpoulpe <43034956+Shhhpoulpe@users.noreply.github.com>
2018-10-11 11:27:25 +0200

    Update gsb.sql

--- gsb.sql
+++ gsb.sql
@@ -137,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `visiteur` (
   `nom` char(30) DEFAULT NULL,
   `prenom` char(30) DEFAULT NULL,
   `login` char(20) DEFAULT NULL,
-  `mdp` char(20) DEFAULT NULL,
+  `mdp` char(40) DEFAULT NULL,
   `adresse` char(30) DEFAULT NULL,
   `cp` char(5) DEFAULT NULL,
   `ville` char(30) DEFAULT NULL,
