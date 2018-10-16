f33211e842d61a8dfdd16e37217e1371ec6ca183
Shhhpoulpe <43034956+Shhhpoulpe@users.noreply.github.com>
2018-10-16 14:06:19 +0200

    Ajout de la colonne année dans la table fichefrais

--- gsb.sql
+++ gsb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 04 oct. 2018 à 11:53
+-- Généré le :  mar. 16 oct. 2018 à 12:04
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
@@ -65,7 +65,15 @@ CREATE TABLE IF NOT EXISTS `fichefrais` (
   KEY `id` (`id`,`mois`,`annee`),
   KEY `fichefrais_ibfk_1` (`idEtat`),
   KEY `fichefrais_ibfk_2` (`idVisiteur`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `fichefrais`
+--
+
+INSERT INTO `fichefrais` (`id`, `idVisiteur`, `mois`, `annee`, `nbJustificatifs`, `montantValide`, `dateModif`, `idEtat`) VALUES
+(1, 'a17', '82018', 2018, 1, '10.00', '2018-05-05', 'CL'),
+(4, 'a17', 'test', 2014, 1, '10.25', '2018-05-05', 'CL');
 
 -- --------------------------------------------------------
 
@@ -107,6 +115,13 @@ CREATE TABLE IF NOT EXISTS `lignefraisforfait` (
   KEY `idFraisForfait` (`idFraisForfait`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `lignefraisforfait`
+--
+
+INSERT INTO `lignefraisforfait` (`idVisiteur`, `mois`, `idFraisForfait`, `quantite`) VALUES
+('a17', 'test', 'ETP', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -123,7 +138,14 @@ CREATE TABLE IF NOT EXISTS `lignefraishorsforfait` (
   `montant` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idVisiteur` (`idVisiteur`,`mois`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `lignefraishorsforfait`
+--
+
+INSERT INTO `lignefraishorsforfait` (`id`, `idVisiteur`, `mois`, `libelle`, `date`, `montant`) VALUES
+(1, 'a17', 'test', 'le test', '2018-05-05', '45.02');
 
 -- --------------------------------------------------------
 
@@ -151,33 +173,33 @@ CREATE TABLE IF NOT EXISTS `visiteur` (
 --
 
 INSERT INTO `visiteur` (`id`, `nom`, `prenom`, `login`, `mdp`, `adresse`, `cp`, `ville`, `dateEmbauche`, `comptable`) VALUES
-('a131', 'Coquerelle', 'Arnaud', 'nono', SHA1('azerty'), '8 rue des Charmes', '46000', 'Cahors', '2002-12-21', 1),
-('a17', 'Andre', 'David', 'dandre', SHA1('oppg5'), '1 rue Petit', '46200', 'Lalbenque', '1998-11-23', 0),
-('a55', 'Jobard', 'Maxime', 'lelilel', SHA1('lelilel12'), '1 rue Peranud', '46250', 'Montcuq', '1995-01-12', 1),
-('a93', 'Tusseau', 'Louis', 'ltusseau', SHA1('ktp3s'), '22 rue des Ternes', '46123', 'Gramat', '2000-05-01', 0),
-('b13', 'Bentot', 'Pascal', 'pbentot', SHA1('doyw1'), '11 allée des Cerises', '46512', 'Bessines', '1992-07-09', 0),
-('b16', 'Bioret', 'Luc', 'lbioret', SHA1('hrjfs'), '1 Avenue gambetta', '46000', 'Cahors', '1998-05-11', 0),
-('b19', 'Bunisset', 'Francis', 'fbunisset', SHA1('4vbnd'), '10 rue des Perles', '93100', 'Montreuil', '1987-10-21', 0),
-('b25', 'Bunisset', 'Denise', 'dbunisset', SHA1('s1y1r'), '23 rue Manin', '75019', 'paris', '2010-12-05', 0),
-('b28', 'Cacheux', 'Bernard', 'bcacheux', SHA1('uf7r3'), '114 rue Blanche', '75017', 'Paris', '2009-11-12', 0),
-('b34', 'Cadic', 'Eric', 'ecadic', SHA1('6u8dc'), '123 avenue de la République', '75011', 'Paris', '2008-09-23', 0),
-('b4', 'Charoze', 'Catherine', 'ccharoze', SHA1('u817o'), '100 rue Petit', '75019', 'Paris', '2005-11-12', 0),
-('b50', 'Clepkens', 'Christophe', 'cclepkens', SHA1('bw1us'), '12 allée des Anges', '93230', 'Romainville', '2003-08-11', 0),
-('b59', 'Cottin', 'Vincenne', 'vcottin', SHA1('2hoh9'), '36 rue Des Roches', '93100', 'Monteuil', '2001-11-18', 0),
-('c14', 'Daburon', 'François', 'fdaburon', SHA1('7oqpv'), '13 rue de Chanzy', '94000', 'Créteil', '2002-02-11', 0),
-('c3', 'De', 'Philippe', 'pde', SHA1('gk9kx'), '13 rue Barthes', '94000', 'Créteil', '2010-12-14', 0),
-('c54', 'Debelle', 'Michel', 'mdebelle', SHA1('od5rt'), '181 avenue Barbusse', '93210', 'Rosny', '2006-11-23', 0),
-('d13', 'Debelle', 'Jeanne', 'jdebelle', SHA1('nvwqq'), '134 allée des Joncs', '44000', 'Nantes', '2000-05-11', 0),
-('d51', 'Debroise', 'Michel', 'mdebroise', SHA1('sghkb'), '2 Bld Jourdain', '44000', 'Nantes', '2001-04-17', 0),
-('e22', 'Desmarquest', 'Nathalie', 'ndesmarquest', SHA1('f1fob'), '14 Place d Arc', '45000', 'Orléans', '2005-11-12', 0),
-('e24', 'Desnost', 'Pierre', 'pdesnost', SHA1('4k2o5'), '16 avenue des Cèdres', '23200', 'Guéret', '2001-02-05', 0),
-('e39', 'Dudouit', 'Frédéric', 'fdudouit', SHA1('44im8'), '18 rue de l église', '23120', 'GrandBourg', '2000-08-01', 0),
-('e49', 'Duncombe', 'Claude', 'cduncombe', SHA1('qf77j'), '19 rue de la tour', '23100', 'La souteraine', '1987-10-10', 0),
-('e5', 'Enault-Pascreau', 'Céline', 'cenault', SHA1('y2qdu'), '25 place de la gare', '23200', 'Gueret', '1995-09-01', 0),
-('e52', 'Eynde', 'Valérie', 'veynde', SHA1('i7sn3'), '3 Grand Place', '13015', 'Marseille', '1999-11-01', 0),
-('f21', 'Finck', 'Jacques', 'jfinck', SHA1('mpb3t'), '10 avenue du Prado', '13002', 'Marseille', '2001-11-10', 0),
-('f39', 'Frémont', 'Fernande', 'ffremont', SHA1('xs5tq'), '4 route de la mer', '13012', 'Allauh', '1998-10-01', 0),
-('f4', 'Gest', 'Alain', 'agest', SHA1('dywvt'), '30 avenue de la mer', '13025', 'Berre', '1985-11-01', 0);
+('a131', 'Coquerelle', 'Arnaud', 'nono', '9cf95dacd226dcf43da376cdb6cbba7035218921', '8 rue des Charmes', '46000', 'Cahors', '2002-12-21', 1),
+('a17', 'Andre', 'David', 'dandre', '12e0b9be32932a8028b0ef0432a0a0a99421f745', '1 rue Petit', '46200', 'Lalbenque', '1998-11-23', NULL),
+('a55', 'Jobard', 'Maxime', 'lelilel', '7f12f36ced4c7ae47d4320a0c8ed36dde3b0dea0', '1 rue Peranud', '46250', 'Montcuq', '1995-01-12', 1),
+('a93', 'Tusseau', 'Louis', 'ltusseau', 'f1c1d39e9898f3202a2eaa3dc38ae61575cd77ad', '22 rue des Ternes', '46123', 'Gramat', '2000-05-01', NULL),
+('b13', 'Bentot', 'Pascal', 'pbentot', '178e1efaf000fdf2267edc43fad2a65197a0ab10', '11 allée des Cerises', '46512', 'Bessines', '1992-07-09', NULL),
+('b16', 'Bioret', 'Luc', 'lbioret', 'ab7fa51f9bf8fde35d9e5bcc5066d3b71dda00d2', '1 Avenue gambetta', '46000', 'Cahors', '1998-05-11', NULL),
+('b19', 'Bunisset', 'Francis', 'fbunisset', 'aa710ca3a1f12234bc2872aa0a6f88d6cf896ae4', '10 rue des Perles', '93100', 'Montreuil', '1987-10-21', NULL),
+('b25', 'Bunisset', 'Denise', 'dbunisset', '40ff56dc0525aa08de29eba96271997a91e7d405', '23 rue Manin', '75019', 'paris', '2010-12-05', NULL),
+('b28', 'Cacheux', 'Bernard', 'bcacheux', '51a4fac4890def1ef8605f0b2e6554c86b2eb919', '114 rue Blanche', '75017', 'Paris', '2009-11-12', NULL),
+('b34', 'Cadic', 'Eric', 'ecadic', '2ed5ee95d2588be3650a935ff7687dee46d70fc8', '123 avenue de la République', '75011', 'Paris', '2008-09-23', NULL),
+('b4', 'Charoze', 'Catherine', 'ccharoze', '8b16cf71ab0842bd871bce99a1ba61dd7e9d4423', '100 rue Petit', '75019', 'Paris', '2005-11-12', NULL),
+('b50', 'Clepkens', 'Christophe', 'cclepkens', '7ddda57eca7a823c85ac0441adf56928b47ece76', '12 allée des Anges', '93230', 'Romainville', '2003-08-11', NULL),
+('b59', 'Cottin', 'Vincenne', 'vcottin', '2f95d1cac7b8e7459376bf36b93ae7333026282d', '36 rue Des Roches', '93100', 'Monteuil', '2001-11-18', NULL),
+('c14', 'Daburon', 'François', 'fdaburon', '5c7cc4a7f0123460c29c84d8f8a73bc86184adbb', '13 rue de Chanzy', '94000', 'Créteil', '2002-02-11', NULL),
+('c3', 'De', 'Philippe', 'pde', '03b03872dd570959311f4fb9be01788e4d1a2abf', '13 rue Barthes', '94000', 'Créteil', '2010-12-14', NULL),
+('c54', 'Debelle', 'Michel', 'mdebelle', '1fa95c2fac5b14c6386b73cbe958b663fc66fdfa', '181 avenue Barbusse', '93210', 'Rosny', '2006-11-23', NULL),
+('d13', 'Debelle', 'Jeanne', 'jdebelle', '18c2cad6adb7cee7884f70108cfd0a9b448be9be', '134 allée des Joncs', '44000', 'Nantes', '2000-05-11', NULL),
+('d51', 'Debroise', 'Michel', 'mdebroise', '46b609fe3aaa708f5606469b5bc1c0fa85010d76', '2 Bld Jourdain', '44000', 'Nantes', '2001-04-17', NULL),
+('e22', 'Desmarquest', 'Nathalie', 'ndesmarquest', 'abc20ea01dabd079ddd63fd9006e7232e442973c', '14 Place d Arc', '45000', 'Orléans', '2005-11-12', NULL),
+('e24', 'Desnost', 'Pierre', 'pdesnost', '8eaa8011ec8aa8baa63231a21d12f4138ccc1a3d', '16 avenue des Cèdres', '23200', 'Guéret', '2001-02-05', NULL),
+('e39', 'Dudouit', 'Frédéric', 'fdudouit', '55072fa16c988da8f1fb31e40e4ac5f325ac145d', '18 rue de l église', '23120', 'GrandBourg', '2000-08-01', NULL),
+('e49', 'Duncombe', 'Claude', 'cduncombe', '577576f0b2c56c43b596f701b782870c8742c592', '19 rue de la tour', '23100', 'La souteraine', '1987-10-10', NULL),
+('e5', 'Enault-Pascreau', 'Céline', 'cenault', 'cc0fb4115bb04c613fd1b95f4792fc44f07e9f4f', '25 place de la gare', '23200', 'Gueret', '1995-09-01', NULL),
+('e52', 'Eynde', 'Valérie', 'veynde', 'd06ace8d729693904c304625e6a6fab6ab9e9746', '3 Grand Place', '13015', 'Marseille', '1999-11-01', NULL),
+('f21', 'Finck', 'Jacques', 'jfinck', '6d8b2060b60132d9bdb09d37913fbef637b295f2', '10 avenue du Prado', '13002', 'Marseille', '2001-11-10', NULL),
+('f39', 'Frémont', 'Fernande', 'ffremont', 'aa45efe9ecbf37db0089beeedea62ceb57db7f17', '4 route de la mer', '13012', 'Allauh', '1998-10-01', NULL),
+('f4', 'Gest', 'Alain', 'agest', '1af7dedacbbe8ce324e316429a816daeff4c542f', '30 avenue de la mer', '13025', 'Berre', '1985-11-01', NULL);
 
 --
 -- Contraintes pour les tables déchargées
