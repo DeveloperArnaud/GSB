23d99b01ece66e4ffa0aa03260f778d2d5c3e202
DeveloperArnaud <43283628+DeveloperArnaud@users.noreply.github.com>
2018-11-15 13:27:25 +0100

    Update c_etatFrais.php

--- gsbMVC_V0/controleurs/c_etatFrais.php
+++ gsbMVC_V0/controleurs/c_etatFrais.php
@@ -1,7 +1,8 @@
-﻿<?php
+<?php
 include("vues/v_sommaire.php");
 $action = $_REQUEST['action'];
 $idVisiteur = $_SESSION['idVisiteur'];
+
 switch($action){
 	case 'selectionnerMois':{
 		$lesMois=$pdo->getLesMoisDisponibles($idVisiteur);
@@ -15,22 +16,28 @@ switch($action){
 		include("vues/v_listeMois.php");
 		break;
 	}
-	case 'voirEtatFrais':{
-		$leMois = $_REQUEST['lstMois']; 
-		$lesMois=$pdo->getLesMoisDisponibles($idVisiteur);
-		$moisASelectionner = $leMois;
-		include("vues/v_listeMois.php");
-		$lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur,$leMois);
-		$lesFraisForfait= $pdo->getLesFraisForfait($idVisiteur,$leMois);
-		$lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur,$leMois);
-		$numAnnee =substr( $leMois,0,4);
-		$numMois =substr( $leMois,4,2);
-		$libEtat = $lesInfosFicheFrais['libEtat'];
-		$montantValide = $lesInfosFicheFrais['montantValide'];
-		$nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
-		$dateModif =  $lesInfosFicheFrais['dateModif'];
-		$dateModif =  dateAnglaisVersFrancais($dateModif);
-		include("vues/v_etatFrais.php");
-	}
+	case 'voirEtatFrais':
+        {
+            $leMois = $_REQUEST['lstMois'];
+            $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+            $moisASelectionner = $leMois;
+            include("vues/v_listeMois.php");
+            $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $leMois);
+            $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $leMois);
+            $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $leMois);
+            $numAnnee = substr($leMois, 0, 4);
+            $numMois = substr($leMois, 4, 2);
+            $libEtat = $lesInfosFicheFrais['libEtat'];
+            $montantValide = $lesInfosFicheFrais['montantValide'];
+            $nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
+            $dateModif = $lesInfosFicheFrais['dateModif'];
+            $dateModif = dateAnglaisVersFrancais($dateModif);
+
+
+            include("vues/v_etatFrais.php");
+        }
+
+
+
 }
 ?>
