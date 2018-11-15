9744b3c47759b3f00b656ae1bdca60a155f43666
DeveloperArnaud <43283628+DeveloperArnaud@users.noreply.github.com>
2018-11-15 12:01:10 +0100

    ajout de fonction permettant de calculer le montant refuse/global

--- gsbMVC_V0/controleurs/c_pdf.php
+++ gsbMVC_V0/controleurs/c_pdf.php
@@ -11,6 +11,11 @@ switch ($action) {
         $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $mois);
         $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $mois);
         $lesInfosFicheFrais = $pdo->getLesInfosFicheFraisPdf($idVisiteur, $mois);
+        $lesInfosUser=$pdo->getLesInfosFicheFraisUser($idVisiteur, $mois);
+        $Lestotal = $pdo->getTotal($idVisiteur,$mois);
+        $refuser = $pdo->getMontantRefuse($idVisiteur,$mois);
+        $totalMontantRef = $pdo->getTotalMontantRefuse($idVisiteur, $mois);
+
 
         include("vues/v_pdf.php");
 }
